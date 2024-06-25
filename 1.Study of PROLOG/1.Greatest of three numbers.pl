0c9d43c52ea3f932bd35cf2bbc66f84277d77374
Pradeesh89 <pradeeshs050905@gmail.com>
2024-06-25 20:34:25 +0530

    correct indentation and order

--- 1.Study of PROLOG/1.Greatest of three numbers.pl
+++ 1.Study of PROLOG/1.Greatest of three numbers.pl
@@ -1,15 +1,17 @@
 PROGRAM:
 
 read_number(Number) :-
-read(Number),
-number(Number).
+    read(Number),
+    number(Number).
+
 main :-
-write('Enter the first number: '),
-read_number(Num1),
-write('Enter the second number: '),
-read_number(Num2),
-Sum is Num1 + Num2,
-format('The sum of ~w and ~w is ~w~n', [Num1, Num2, Sum]).
+    write('Enter the first number: '),
+    read_number(Num1),
+    write('Enter the second number: '),
+    read_number(Num2),
+    Sum is Num1 + Num2,
+    format('The sum of ~w and ~w is ~w~n', [Num1, Num2, Sum]).
+
 
 
 QUERIES:
--- 1.Study of PROLOG/2.Minimum and Maximum of two numbers.pl
+++ 1.Study of PROLOG/2.Minimum and Maximum of two numbers.pl
@@ -1,20 +1,25 @@
 PROGRAM:
+
 read_number(Number) :-
-read(Number),
-number(Number).
+    read(Number),
+    number(Number).
+
 max_number(X, Y, Max) :-
-(X >= Y -> Max = X ; Max = Y).
+    (X >= Y -> Max = X ; Max = Y).
+
 min_number(X, Y, Min) :-
-(X =< Y -> Min = X ; Min = Y).
+    (X =< Y -> Min = X ; Min = Y).
+
 main :-
-write('Enter the first number: '),
-read_number(Num1),
-write('Enter the second number: '),
-read_number(Num2),
-max_number(Num1, Num2, Max),
-min_number(Num1, Num2, Min),
-format('The maximum of ~w and ~w is ~w~n', [Num1, Num2, Max]),
-format('The minimum of ~w and ~w is ~w~n', [Num1, Num2, Min]).
+    write('Enter the first number: '),
+    read_number(Num1),
+    write('Enter the second number: '),
+    read_number(Num2),
+    max_number(Num1, Num2, Max),
+    min_number(Num1, Num2, Min),
+    format('The maximum of ~w and ~w is ~w~n', [Num1, Num2, Max]),
+    format('The minimum of ~w and ~w is ~w~n', [Num1, Num2, Min]).
+
 
 
 QUERIES:
